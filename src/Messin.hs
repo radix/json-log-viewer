10cece62f4db73ca3bf870de34840f076c869596
Christopher Armstrong <radix@twistedmatrix.com>
2015-04-15 00:33:10 -0500

    use init not allButLast

--- src/Messin.hs
+++ src/Messin.hs
@@ -22,13 +22,6 @@ import           System.Posix.Files      (fileSize, getFileStatus)
 type FilePosition = Integer
 type MicroSeconds = Int
 
--- Not sure if this allButLast thing is that great
-
-allButLast :: [a] -> [a]
-allButLast (x:[]) = []
-allButLast (x:xs) = (x:allButLast xs)
-allButLast []     = []
-
 -- TODO: Implement `tail -F` behavior -- streamLines is `tail -f`
 
 -- |Tail a file, sending complete lines to the passed-in IO function.
@@ -58,7 +51,7 @@ streamLines path sizeSoFar delay callback = go sizeSoFar
            newContents <- BS.hGetContents handle
            let lines = BS.splitWith (==10) newContents
            let startNext = newSize - (toInteger $ BS.length $ last lines)
-           mapM_ (callback . Right) $ allButLast lines
+           mapM_ (callback . Right) $ init lines
            go startNext
          else do
            go sizeSoFar
